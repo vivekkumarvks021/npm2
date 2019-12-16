f846134901ce148d55c02c36148723308493ddfc
Vivek Kumar <vivekkumarvks021@gmail.com>
2019-12-16 21:55:57 +0530

    deploy_npm_module

---
+++ demo.js
@@ -0,0 +1,10 @@
+/* var export_date= function(){
+    let date1 = new Date();
+    return date1;
+} 
+console.log(export_date());
+*/
+ module.exports.derived=function(){
+    var date1= new Date();
+    return date1;
+}
